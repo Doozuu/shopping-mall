33041c479332c47a14b03b5942b9c41fcc28e631
LJH <049494@ewhain.net>
2023-02-15 17:04:25 +0900

    feat : adding items to cart

--- src/ components/cart-icon/cart-icon.component.jsx
+++ src/ components/cart-icon/cart-icon.component.jsx
@@ -7,12 +7,17 @@ import { CartContext } from "../../contexts/cart.context";
 import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartItems, cartCount } =
+    useContext(CartContext);
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">
+        {cartCount}
+        {/* {cartItems.reduce((acc, cur) => acc + cur.quantity, 0)} */}
+        {/* useContext를 이용해 carCount를 세는 대신에 위처럼만 해도 된다. */}
+      </span>
     </div>
   );
 };
